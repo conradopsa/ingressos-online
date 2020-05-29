2470bd2b84207c8c2990041eccbe02b33b2758a4
Conrado Pinheiro <conradopsa@gmail.com>
2020-05-29 00:04:43 -0300

    Implementação autenticação

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
+function checarLogin(){
+    let cliente = localStorage.getItem("cliente");
+    let token = cliente.token;
+
+    if (token){
+        $("#menuCadastro").hide();
+        $("#menuLogin").hide();
+    }  
+}
+
 //Pagina pricipal
 setarPagina("html/paginas/home.html")
 
@@ -8,3 +18,5 @@ $("#menuHome").on('click', () => setarPagina('html/paginas/home.html'));
 $("#menuLogin").on('click', () => setarPagina('html/paginas/login.html'));
 $("#menuCadastro").on('click', () => setarPagina('html/paginas/cadastro.html'));
 $("#menuSobre").on('click', () => setarPagina('html/paginas/sobre.html'));
+
+checarLogin();
