c9c07074846c78efccbbb12fc58f148d8e19e56d
Bianca Barbosa <bianca-barbosa@zoom.com.br>
2020-05-28 23:18:53 -0300

    tela de cadastro

--- js/controllers/paginas/CadastroController.js
+++ js/controllers/paginas/CadastroController.js
@@ -1,1 +1,27 @@
 
+'use strict';
+
+function cadastrar() {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    let clientePromise = fetch(`http://localhost:3000/auth`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            senha: senha
+        })
+    })
+        .then((res) => res.json().then(cliente => cliente))
+        .catch((err) => console.log(err));
+
+    clientePromise.then(cliente => {
+        console.log(cliente);
+    })
+
+    //$(".alert").alert()
+}
